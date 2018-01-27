386faf9516f59840f23e3a9bbf207b22c3552182
Bruno M. Mergen <brunomergen@gmail.com>
2018-01-27 16:24:56 -0200

    Challenge: Created test for Raffle Service where test that a member x dont get a member y that get the member x. Challenge accepted :sunglasses:

--- spec/services/raffle_service_spec.rb
+++ spec/services/raffle_service_spec.rb
@@ -1,58 +1,55 @@
 require 'rails_helper'
 
 describe RaffleService do
-
-  before :each do
-    @campaign = create(:campaign, status: :pending)
-  end
+  let(:campaign) { build(:campaign, status: :pending) }
+  let(:results) { RaffleService.new(campaign).call }
 
   describe '#call' do
     context "when has more then two members" do
-      before(:each) do
-        create(:member, campaign: @campaign)
-        create(:member, campaign: @campaign)
-        create(:member, campaign: @campaign)
-        @campaign.reload
 
-        @results = RaffleService.new(@campaign).call
+      before(:each) do
+        4.times.map{ |i|  campaign.members.build(attributes_for(:member)) }
+        allow(campaign.members).to receive(:count) { 3 } # Stubs campaign.members count
       end
 
       it "results is a hash" do
-        expect(@results.class).to eq(Hash)
+        expect(results.class).to eq(Hash)
       end
 
       it "all members are in results as a member" do
-        result_members = @results.map {|r| r.first}
-        expect(result_members.sort).to eq(@campaign.members.sort)
+        result_members = results.map {|r| r.first}
+        expect(result_members).to match_array(campaign.members.to_a)
       end
 
       it "all member are in results as a friend" do
-        result_friends = @results.map {|r| r.last}
-        expect(result_friends.sort).to eq(@campaign.members.sort)
+        result_friends = results.map {|r| r.last}
+        expect(result_friends).to match_array(campaign.members.to_a)
       end
 
       it "a member don't get yourself" do
-        @results.each do |r|
+        results.each do |r|
           expect(r.first).not_to eq(r.last)
         end
       end
 
-      # Desafio
-      # TODO
-      it 'a member x dont get a member y that get the member x'
+      # Desafio - Challenge accepted 8-]
+      it 'a member x dont get a member y that get the member x' do
+        result_members = results.map {|r| r.first}
+        result_members.each do |member|
+          friend = results[member]
+          expect(results[friend]).not_to eq(member)
+        end
+      end
 
     end
 
     context "when don't has more then two members" do
       before(:each) do
-        create(:member, campaign: @campaign)
-        @campaign.reload
-
-        @response = RaffleService.new(@campaign).call
+        allow(campaign.members).to receive(:count) { 2 } # Stubs campaign.members count
       end
 
       it "raise error" do
-        expect(@response).to eql(false)
+        expect(results).to eql(false)
       end
     end
   end
